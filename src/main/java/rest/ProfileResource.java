28942fb6c32e8b7e64838aa330b0eafe63b439ad
Morten Bendeke <mbendeke@hotmail.com>
2022-11-24 13:08:45 +0100

    created ProfileResource

---
+++ src/main/java/rest/ProfileResource.java
@@ -0,0 +1,50 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.ProfileDto;
+import entities.User;
+import facades.ProfileFacade;
+import utils.EMF_Creator;
+
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
+
+@Path("profile")
+public class ProfileResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final ProfileFacade FACADE =  ProfileFacade.getInstance(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createProfile(String content) {
+        ProfileDto profileDtoFromJSON = GSON.fromJson(content, ProfileDto.class);
+         ProfileDto profileDto = FACADE.createProfile(profileDtoFromJSON);
+
+         return Response.ok().entity(GSON.toJson(profileDto)).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id]")
+    @RolesAllowed("admin")
+    public String getFromAdmin() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
+    }
+}
